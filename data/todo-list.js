30f158516521ca365e0cab8ac37945e183612904
Zach Butler <zach.standley93@gmail.com>
2019-05-20 16:58:00 -0700

    made todo list

---
+++ data/todo-list.js
@@ -0,0 +1,16 @@
+const todos = [
+    {
+        task: 'Learn templates',
+        completed: true
+    },
+    {
+        task: 'Learn forEach',
+        completed: false
+    },
+    {
+        task: 'Learn flexbox',
+        completed: false
+    }
+];
+
+export default todos;
