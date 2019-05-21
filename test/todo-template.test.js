e2cb058e81b1b257f2eb2e0d59333861e21e7c27
Zach Butler <zach.standley93@gmail.com>
2019-05-20 17:48:55 -0700

    TDD todolist template function passing

--- test/todo-template.test.js
+++ test/todo-template.test.js
@@ -1,10 +1,20 @@
 const test = QUnit.test;
 
 function template(todo) {
+    let checked = '';
 
+    if(todo.completed) {
+        checked = 'checked';
+    }
+
+    return /*html*/`
+    <li>
+        <input type="checkbox" ${checked}> <span>${todo.task}</span>
+    </li>
+`;
 }
 
-test('is true if checked', function(assert) {
+test('is true is checked', function(assert) {
     //Arrange
     const todo = {
         task: 'Learn templates',
@@ -15,6 +25,26 @@ test('is true if checked', function(assert) {
         <li>
             <input type="checkbox" checked> <span>Learn templates</span>
         </li>
+`;
+    //Act 
+    const html = template(todo);
+    
+
+    //Assert
+    assert.htmlEqual(html, expected);
+});
+
+test('is false not checked', function(assert) {
+    //Arrange
+    const todo = {
+        task: 'Learn forEach',
+        completed: false
+    };
+
+    const expected = /*html*/`
+    <li>
+        <input type="checkbox" > <span>Learn forEach</span>
+    </li>
 `;
     //Act 
     const html = template(todo);
